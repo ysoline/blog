37dc4d47c5ebdf4a0a998ceb9d585263dce0980d
Julie Pilarski <jpi@snel.local>
2019-06-18 14:49:06 +0200

    Ajout du fichier template.php

---
+++ view/frontend/template.php
@@ -0,0 +1,16 @@
+<!doctype html>
+    <head>
+    <meta charset="utf-8"
+    <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/flatly/bootstrap.min.css" rel="stylesheet" integrity="sha384-T5jhQKMh96HMkXwqVMSjF3CmLcL1nT9//tCqu9By5XSdj7CwR0r+F3LTzUdfkkQf" crossorigin="anonymous">
+
+    <title><?= $title ?></title>
+
+    </head>
+
+    <body>
+    <? $content ?>
+    </body>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
+    
+</html>
