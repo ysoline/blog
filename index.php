ed373a85633dd7c9d0300f7c89ef1e2daab6a0c5
ysoh <juliepilarski@yahoo.fr>
2019-06-18 19:30:01 +0200

    ajout d'action inscription/connexion

--- index.php
+++ index.php
@@ -2,11 +2,20 @@
 
     require ('controller/postController.php');
     require ('controller/commentController.php');
-    require ('view/frontend/souscribeView.php');   
+    require ('view/frontend/indexView.php');
     //require ('controller/userController.php');
 
     try{
-        
+        if(isset($_GET['action'])){
+            if($_GET['action']=='suscribe'){
+                header('Location:view/frontend/souscribeView.php'); //inscription
+            }
+            if($GET['action'] == 'newUSer'){
+                if(!empty($_POST['pseudo']) && !empty($_POST['password'])){
+                    
+                }
+            }
+        }
 
     }
     catch(Exception $e) {
