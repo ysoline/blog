217db1b4641416d565d1cd83e0df9c9bbdf550bf
ysoh <juliepilarski@yahoo.fr>
2019-06-29 19:18:29 +0200

    Modification autoloarder pour charger toutes les classes

--- Autoloader.php
+++ Autoloader.php
@@ -1,16 +1,25 @@
 <?php
 
 
- class Autoloader{
+ class Autoloader
+ {
+     public static function register()
+     {
+         spl_autoload_register(function ($class_name) {
 
-
-    static function register(){
-        spl_autoload_register(array(__CLASS__, 'autoload'));
-    }
-
-    static function autoload($class){
-        require( __DIR__.'/'.$class. '.php');           
-       
-    }
-
-}
+  
+             $dirs = array(
+        'Controllers/',
+        'Models/',
+        'Models/Manager/',
+        
+    );
+             foreach ($dirs as $dir) {
+                 if (file_exists($dir.$class_name.'.php')) {
+                     require_once($dir.$class_name.'.php');
+                     return;
+                 }
+             }
+         });
+     }
+ }
