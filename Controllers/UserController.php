755460f79d75b3e7c96a6c927f55c65f1f7a53bb
ysoh <juliepilarski@yahoo.fr>
2019-07-01 22:40:33 +0200

    function addUser: ajout des vérifications à faire

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -6,14 +6,45 @@ class UserController
 {
     public function newUser($pseudo, $pass1, $pass2, $email, $email2)
     {
-        $userManager= new User;
-        $newUser= $userManager-> createUser($pseudo, $pass1, $pass2, $email, $email2);
+        $userManager = new User;
 
-        if ($newUser === false){
-            throw new Exception('Inscription impossible');
+        $pseudolenght = strlen($pseudo);
+        if ($pseudolenght < 5 || $pseudolenght > 15) {
+            $pseudo->pseudoExist();
+            if ($pseudo == 0) {
+                if ($pass1 == $pass2) {
+                    $pass = password_hash($pass1, PASSWORD_DEFAULT);
+                    if (
+                        $email == $email2
+                    ) {
+                        $newUser = $userManager->createUser($pseudo, $pass, $email);
+                    } else {
+                        throw new Exception("Les emails ne sont pas identiques");;
+                    }
+                } else {
+                    throw new Exception('Les mots de passes ne sont pas identiques');
+                }
+            } else {
+                throw new Exception('Le pseudo existe déjà');
+            }
+        } else {
+            throw new Exception("La longueur du pseudo doit être comprise entre 5 et 15 caractères");
         }
-        else{
-            echo 'Enregistré !';
-        }
+    }
+
+    public function getUser($id)
+    {
+        $userManager = new User;
+        $user = $userManager->getUser($id);
+
+        return $user;
+    }
+
+    public function editUser($id)
+    {
+        $userManager = new User;
+        $editUser = $userManager->editUser($id);
+
+        return $editUser;
     }
 }
