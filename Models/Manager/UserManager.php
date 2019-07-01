6d6213830ef25e9798d59bfe245d5f53411028be
ysoh <juliepilarski@yahoo.fr>
2019-07-01 22:39:52 +0200

    Ajout d'une fonction verifyPseudo

--- Models/Manager/UserManager.php
+++ Models/Manager/UserManager.php
@@ -1,22 +1,28 @@
 <?php
 
-
-
 class UserManager extends Manager {
 
-    public function createUser($pseudo, $pass1, $pass2, $email, $email2) //ajouter un utilisateur lors de l'inscription
+    public function addUser() //ajouter un utilisateur lors de l'inscription
     {
-        $_bdd=$this->getDbConnect();
+        $_bdd=$this->dbConnect();
         $reqUser= $_bdd->prepare('INSERT INTO users(pseudo, pass1, pass2, email, email2) VALUES(?,?,?,?,?) ');
-        $newUser = $reqUser -> execute(array($pseudo, $pass1, $pass2, $email, $email2));
+        $newUser = $reqUser -> execute(array());
 
         return $newUser; 
     }
 
+    public function pseudoExist($pseudo)
+    {
+        $_bdd=$this->dbConnect();
+        $pseudoExist = $_bdd->prepare('SELECT pseudo FROM users WHERE pseudo=?');
+        $pseudoExist->execute(array($pseudo));
+
+        return $pseudoExist;
+    }
 
     public function getUser($pseudo, $password) //selectionner un utilisateur (verification) lors de la connexion
     {
-        $_bdd=$this->getDbConnect();
+        $_bdd=$this->dbConnect();
         $req= $_bdd->prepare('SELECT $id, $pseudo, password FROM users WHERE pseudo=?');
         $user=$req->execute(array($pseudo, $password));
 
@@ -25,16 +31,16 @@ class UserManager extends Manager {
 
     private function editUser($pseudo, $password)
     {
-        $_bdd=$this->getDbConnect();
+        $_bdd=$this->dbConnect();
         $data= $_bdd->prepare('UPDATE users set pseudo =?, password=? WHERE id=?');
         $data->execute(array($pseudo, $password));
 
         return $data;
     }
 
-    private function deleteUser($pseudo, $password, $email)
-    {
-        $_bdd=$this->getDbConnect();
-        $req= $_bdd->exec('DELETE FROM users WHERE pseudo=?, password=?, email=?');
-    }
+    // private function deleteUser($pseudo, $password, $email)
+    // {
+    //     $_bdd=$this->dbConnect();
+    //     $req= $_bdd->exec('DELETE FROM users WHERE pseudo=?, password=?, email=?');
+    // }
 }
