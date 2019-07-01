7326bd49d4d3b4e667a042d15c71eb24be379bd0
ysoh <juliepilarski@yahoo.fr>
2019-07-01 22:40:59 +0200

    Changement des types des imputs (mal paramétrés)

--- Views/Frontend/suscribeView.php
+++ Views/Frontend/suscribeView.php
@@ -12,21 +12,21 @@
 
         <div class="d-flex justify-content-center">
             <label for ="password">Mot de passe : </label>
-            <input type="text" id="pass1" name="pass1"/>
+            <input type="password" id="pass1" name="pass1"/>
         </div>
         <div class="d-flex justify-content-center">
             <label for ="password">Confirmation mot de passe : </label>
-            <input type="text" id="pass2" name="pass2"/>
+            <input type="password" id="pass2" name="pass2"/>
         </div>
 
         <div class="d-flex justify-content-center">
             <label for ="email">Mail : </label>
-            <input type="text" id="email" name="email"/>
+            <input type="email" id="email" name="email"/>
         </div>  
         
         <div class="d-flex justify-content-center">
             <label for ="email">Confirmation mail : </label>
-            <input type="text" id="email2" name="email2"/>
+            <input type="email" id="email2" name="email2"/>
         </div>
 
         <div>
