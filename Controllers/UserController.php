3e88dbdfe6a2633f6414cc118522bc28cf4d0144
ysoh <juliepilarski@yahoo.fr>
2019-07-14 12:46:20 +0200

    Déplacement des fonctionnalités qui sert à l'authentification (nouveaux fichiers créés)

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -1,39 +1,8 @@
 <?php
 
 //regroupe les fonctionnalités lié aux utillisateurs 
-// inscription/connexion... 
+// modification mot de passe, changement de pseudo, édition profil...
 class UserController
 {
-    public function newUser($pseudo, $pass, $email)
-    {
-        $userManager = new UserManager;
-        $newUser = $userManager -> addUser($pseudo, $pass, $email);
-
-        header('Location: index.php?action=listPosts');
-    }
-
-    // public function editUser($id)
-    // {
-    //     $userManager = new User;
-    //     $editUser = $userManager->editUser($id);
-
-    //     return $editUser;
-    // }
-
-    public function connectUser($pseudo, $pass)
-    {
-        $coUser = new UserManager;
-        $coUser -> getUser($pseudo, $pass);
-       
-        header('Location: index.php?action=listPosts');
-    }
-
-    public function connectPage()
-    {
-        require('Views/Frontend/authView.php');
-    }
-    public function suscribePage()
-    {
-        require('Views/Frontend/suscribeView.php');
-    }
+   
 }
--- Models/Manager/UserManager.php
+++ Models/Manager/UserManager.php
@@ -2,38 +2,6 @@
 
 class UserManager extends Manager {
 
-    public function addUser($pseudo, $pass, $email) //ajouter un utilisateur lors de l'inscription
-    {
-        $newUser=[];
-        $pass_hach=password_hash($pass, PASSWORD_DEFAULT);
-
-        $_bdd=$this->dbConnect();
-        $reqUser= $_bdd->prepare('INSERT INTO users(pseudo, pass, email) VALUES(:pseudo,:pass,:email)');
-        $newUser = $reqUser -> execute(array(
-            'pseudo'=>$pseudo, 
-            'pass'=>$pass_hach, 
-            'email'=>$email));
-
-        return $newUser; 
-    }
-
-    public function getUser($pseudo, $pass) //selectionner un utilisateur (verification) lors de la connexion
-    {
-        $_bdd=$this->dbConnect();
-        $req= $_bdd->prepare('SELECT id, pseudo, pass FROM users WHERE pseudo=?');
-        
-        $count = $req-> rowCount(); //Vérification de l'existence d'un pseudo;
-
-        if($count == 1)
-        {
-            return $count;
-        }
-        else {
-            throw new Exception("Identifiants inconnu");
-            
-        }         
-     }
-
     // public function editUser($pseudo, $pass)
     // {
     //     $_bdd=$this->dbConnect();
