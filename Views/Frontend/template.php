0b840f08bb3cc653ffbfcd5858aabbe4dd254396
ysoh <juliepilarski@yahoo.fr>
2019-07-29 23:04:57 +0200

    ajout de script javascript

--- Views/Frontend/template.php
+++ Views/Frontend/template.php
@@ -1,43 +1,60 @@
 <!DOCTYPE html>
 <html>
-    <head>
+
+<head>
     <meta charset="utf-8">
-    <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/lux/bootstrap.min.css" rel="stylesheet" integrity="sha384-hVpXlpdRmJ+uXGwD5W6HZMnR9ENcKVRn855pPbuI/mwPIEKAuKgTKgGksVGmlAvt" crossorigin="anonymous">  
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> 
-    <title ><?= $title= "Jean Forteroche" ?></title>
-    </head>        
-    <body>
+    <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/lux/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-hVpXlpdRmJ+uXGwD5W6HZMnR9ENcKVRn855pPbuI/mwPIEKAuKgTKgGksVGmlAvt" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <title><?= $title = "Jean Forteroche" ?></title>
+</head>
+
+<body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="index.php">Jean Forteroche</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <ul class="navbar-nav mr-auto">
-    
-  <?php if(!isset($_SESSION['pseudo'])) {?>
-    <li class="nav-item active">
-          <a class="nav-link" href="index.php?action=auth">Connexion</a>
-    </li>
-    <li class="nav-item active">
-          <a class="nav-link" href="index.php?action=suscribePage">Inscription</a>
-    </li>
-    <?php } ?>
+        <a class="navbar-brand" href="index.php">Jean Forteroche</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02"
+            aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <ul class="navbar-nav mr-auto">
 
-    <?php if(isset($_SESSION['pseudo'])) {?>
-    <li class="nav-item active">
-          <a class="nav-link" href="index.php?action=profil">Profil</a>
-    </li>
-    <li class="nav-item active">
-          <a class="nav-link" href="index.php?action=disconnect">Déconnexion</a>
-    </li>
-    <?php } ?>
-  </ul>
-</nav>
+            <?php if (!isset($_SESSION['pseudo'])) { ?>
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php?action=auth">Connexion</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php?action=suscribePage">Inscription</a>
+            </li>
+            <?php } ?>
 
-        <?= $content ?>
+            <?php if (isset($_SESSION['pseudo'])) { ?>
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php?action=profil">Profil</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php?action=disconnect">Déconnexion</a>
+            </li>
+            <?php } ?>
+        </ul>
+    </nav>
 
-    </body>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
-    
+    <?= $content ?>
+
+</body>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"
+    integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous">
+</script>
+
 </html>
