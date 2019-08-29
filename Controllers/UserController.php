db27d0f81d06a68e6e4cff7a6efc8a8d0eaf607f
Julie Pilarski <jpi@snel.local>
2019-08-29 11:59:08 +0200

    Verification des infos avant édition de profil

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -17,13 +17,16 @@ class UserController
      *
      * @return void
      */
-    public function editProfil($id, $pseudo, $email)
+    public function editProfil()
     {
-        $id = $_SESSION['id_user'];
-
-        $userEdit = new UserManager;
-        $changePseudo = $userEdit->editProfil($id, $pseudo, $email);
-        header('Location: index.php?action=profil');
+        $userManager = new UserManager;
+        $infoUser = $userManager->getInfo($_SESSION['id_user']);
+        if ($_SESSION['id_user'] == $infoUser['id']) {
+            $changePseudo = $userManager->editProfil($_SESSION['id_user'], $_POST['pseudo'], $_POST['email']);
+            header('Location: index.php?action=profil');
+        } else {
+            throw new Exception('Vous n\'êtes pas autorisé à faire cela');
+        }
     }
 
     /**
@@ -31,25 +34,37 @@ class UserController
      *
      * @return void
      */
-    public function editPass($id, $pass)
+    public function editPass()
     {
-        $_POST['pass'] = password_hash(htmlspecialchars($_POST['pass']), PASSWORD_DEFAULT);
-        $userEdit = new UserManager;
-        $changePseudo = $userEdit->editPass($_GET['id'], htmlspecialchars($_POST['pass']));
-        header('Location: index.php?action=profil');
+        $userManager = new UserManager;
+
+        $infoUser = $userManager->getInfo($_SESSION['id_user']);
+        if ($_SESSION['id_user'] == $infoUser['id']) {
+            if ($_POST['pass'] == $_POST['pass2']) {
+                $_POST['pass'] = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+                $changePseudo = $userManager->editPass($_GET['id'], $_POST['pass']);
+                header('Location: index.php?action=profil');
+            } else {
+                throw new Exception('Les mots de passe ne sont pas identiques <br/><a href="index.php?action=profil">Retour</a>');
+            }
+        } else {
+            throw new Exception('Vous n\'êtes pas autorisé à faire cela');
+        }
     }
     /**
      * Suppression d'un compte utilisateur
      *
-     * @param mixed $id
      * @return void
      */
-    public function deleteUser($id)
+    public function deleteUser()
     {
-        $id = $_SESSION['id_user'];
-
-        $deleteUser = new UserManager;
-        $deleteUser->deleteUser($id);
-        header('Location: index.php?action=profil');
+        $userManager = new UserManager;
+        $infoUser = $userManager->getInfo($_SESSION['id_user']);
+        if ($_SESSION['id_user'] == $infoUser['id']) {
+            $deleteUser = $userManager->deleteUser($_SESSION['id_user']);
+            header('Location: index.php?action=profil');
+        } else {
+            throw new Exception('Vous n\'êtes pas autorisé à faire cela');
+        }
     }
 }
