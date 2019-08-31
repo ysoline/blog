dceb7b1f50752763d003a31427a857c1339bdfe5
ysoh <juliepilarski@yahoo.fr>
2019-08-31 21:05:35 +0200

    Mise en place d'un slider

--- Views/Frontend/template.php
+++ Views/Frontend/template.php
@@ -4,87 +4,117 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous" />
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.tiny.cloud/1/umdi5wzy8syfdqdun003ztwbhuop2jj1p42n75ixljkydgf1/tinymce/5/tinymce.min.js">
     </script>
     <script>
-        tinymce.init({
-            selector: '#post',
-            language_url: '../../public/tinymce/langs/fr_FR.js'
-        });
+    tinymce.init({
+        selector: '#post',
+        language_url: '../../public/tinymce/langs/fr_FR.js'
+    });
     </script>
     <title><?= $title = "Jean Forteroche" ?></title>
 </head>
-<style>
-    body {
-        background-image: url("public/img/bg_alaska.jpg");
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-        background-size: cover;
-    }
-</style>
-<div class="container-fluid">
-    <header class="blog-header">
 
-        <div class="collapse" id="navbarToggleExternalContent">
-            <div class="d-flex justify-content-between align-items-center p-4 rounded border">
-                <a class="text-light" href="index.php?action=listPosts">
-                    <h1>Jean Forteroche</h1>
-                </a>
-
-                <?php if (!isset($_SESSION['id_user'])) { ?>
-                <a class="text-light" href="index.php?action=suscribePage">Inscription</a>
-                <?php } ?>
-                <?php if (isset($_SESSION['id_user'])) { ?>
-                <a class="text-light text-decoration-none mx-2" href="index.php?action=profil">Profil</a>
-
-                <?php if (($_SESSION['rank']) == 1) { ?>
-                <a class="text-light" href="index.php?action=panelAdmin">Administration</a>
-                <?php }
-                } ?>
-
-                </a>
-                <?php if (!isset($_SESSION['id_user'])) { ?>
-                <a class="btn btn-sm btn-outline-light" href="index.php?action=auth">Connexion</a>
-                <?php } ?>
-                <?php if (isset($_SESSION['id_user'])) { ?>
-                <a class="btn btn-sm btn-outline-light" href="index.php?action=disconnect">Déconnexion</a>
-                <?php } ?>
+<header class="blog-header">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="index.php?action=listPosts">Jean Forteroche</a>
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <div class="d-flex flex-column flex-sm-row justify-content-between col-lg-11">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <?php if (!isset($_SESSION['id_user'])) { ?>
+                        <a class="nav-link" href="index.php?action=suscribePage"><span
+                                class="glyphicon glyphicon-user">Inscription</a>
+                        <?php } ?>
+                    </li>
+                    <li>
+                        <?php if (isset($_SESSION['id_user'])) { ?>
+                        <a class="nav-link text-decoration-none mx-2" href="index.php?action=profil">Profil</a>
+                    </li>
+                    <li>
+                        <?php if (($_SESSION['rank']) == 1) { ?>
+                        <a class="nav-link" href="index.php?action=panelAdmin">Administration</a>
+                        <?php }
+                    } ?>
+                </ul>
+                <ul class="nav navbar-nav">
+                    <li>
+                        <?php if (!isset($_SESSION['id_user'])) { ?>
+                        <a class="btn btn-sm btn-outline-light" href="index.php?action=auth"><i
+                                class="fas fa-user-alt"></i>
+                            Connexion</a>
+                        <?php } ?>
+                    </li>
+                    <li>
+                        <?php if (isset($_SESSION['id_user'])) { ?>
+                        <a class="btn btn-sm btn-outline-light" href="index.php?action=disconnect"><i
+                                class="fas fa-power-off"></i> Déconnexion</a>
+                        <?php } ?>
+                    </li>
+                </ul>
             </div>
         </div>
-
-        <nav class="nav navbar-light">
-            <button class="navbar-toggler font-weight-bold" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span> Menu
-            </button>
-        </nav>
-        <div>
-            <div class=" px-5 mt-2">
-                <div class="col-lg d-flex justify-content-center align-items-center card p-0">
-                    <img src="public\img\aigle_alaska.png" class="card-img rounded shadow d-none d-lg-block" alt='aigle_alaska'>
-                    <h2 class="font-italic text-center text-light font-weight-bold card-img-overlay d-none d-lg-block">Billet simple pour l'Alaska</h2>
+    </nav>
+    <div>
+        <div id="carousel_alaska" class="carousel slide carousel-fade" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carousel_alaska" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel_alaska" data-slide-to="1"></li>
+                <li data-target="#carousel_alaska" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="public\img\1.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="public\img\2.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="public\img\3.jpg" alt="Third slide">
                 </div>
             </div>
-            <h1 class="font-italic text-center  font-weight-bold d-lg-none">Billet simple pour l'Alaska</h1>
-
-    </header>
-
-    <body>
-        <div class="container-fluid mt-2">
-            <?= $content ?>
+            <a class="carousel-control-prev" href="#carousel_alaska" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Précedente</span>
+            </a>
+            <a class="carousel-control-next" href="#carousel_alaska" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Suivante</span>
+            </a>
         </div>
-    </body>
 
-    <footer class="blog-footer text-center px-5">
-        <p class="text-light">Copyright © Julie Pilarski - 2019 - Tous droits réservés</p>
-        <small class="text-light">Projet 4 - Formation Développeur Web Junior</small>
-    </footer>
+        <div class="text-center m-4">
+            <h1>"Billet simple pour l'Alaska"<br /><small class="text-muted"> par </small>Jean Forteroche</h1>
+        </div>
+</header>
 
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous">
-    </script>
+<body>
+    <div class="container-fluid mt-2 py-4">
+        <?= $content ?>
+    </div>
+</body>
+
+<footer class="blog-footer bg-dark text-center p-3">
+    <p class="text-light">Copyright © Julie Pilarski - 2019 - Tous droits réservés</p>
+    <small class="text-light">Projet 4 - Formation Développeur Web Junior</small>
+</footer>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+</script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"
+    integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous">
+</script>
 
 </html>
