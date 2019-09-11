ed98dbb468f55f7d3c9b5a5364b977a0294572a1
ysoh <juliepilarski@yahoo.fr>
2019-09-11 21:05:39 +0200

    Mise à jour Autoloader

--- Autoloader.php
+++ Autoloader.php
@@ -1,25 +1,24 @@
 <?php
 
 
- class Autoloader
- {
-     public static function register()
-     {
-         spl_autoload_register(function ($class_name) {
+class Autoloader
+{
+    public static function register()
+    {
+        spl_autoload_register(function ($class_name) {
 
-  
-             $dirs = array(
-        'Controllers/',
-        'Models/',
-        'Models/Manager/',
-        
-    );
-             foreach ($dirs as $dir) {
-                 if (file_exists($dir.$class_name.'.php')) {
-                     require_once($dir.$class_name.'.php');
-                     return;
-                 }
-             }
-         });
-     }
- }
+
+            $dirs = array(
+                'Controllers/',
+                'Models/Manager/',
+
+            );
+            foreach ($dirs as $dir) {
+                if (file_exists($dir . $class_name . '.php')) {
+                    require_once($dir . $class_name . '.php');
+                    return;
+                }
+            }
+        });
+    }
+}
